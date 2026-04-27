09d8848e117e6d8dde542e9c1af06977e1ebc2db
hyesun <anhs_0218@naver.com>
2026-04-28 01:16:53 +0900

    fix: App.tsx 미사용 import 제거 (useAuth)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider, useAuth } from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext'
 import Login from './pages/Login'
 import Booth from './pages/Booth'
 import General from './pages/General'
